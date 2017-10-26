6ee8dfe713eb875c397beb9d5af7ceb6d65d6c40
abhijit.kashyap07 <abhijit.kashyap07@gmail.com>
2017-10-25 23:12:19 -0400

    added dialogflow intents in strings module

---
+++ strings.py
@@ -0,0 +1,8 @@
+#dialogflow
+
+CONJUGATE_INTENT = "conjugate"
+GENDER_INTENT = "genderWord"
+PRONOUNCE_INTENT = "pronounce"
+TRANSLATE_INTENT = "translate"
+SENTENCE_INTENT = "useInSentence"
+WORD_INTENT = "defineWord"
