79eb982ecad26c84d070df98f3a03dd290802cea
omijn <abhijit.kashyap07@gmail.com>
2018-05-09 12:10:14 -0400

    changed scraper to use section headers as identifiers instead of table number on page

--- language/conjugator.py
+++ language/conjugator.py
@@ -5,83 +5,99 @@ import wiki_telecharger_fr as wtf
 tenses = [
 	{	
 		"code": 0,
+		"section_code": 0,
 		"mode": strings.MODES["IND"],		
-		"name": strings.TENSES["IND_PRES"]
+		"name": strings.TENSES["IND_PRES"]		
 	},
 	{	
 		"code": 1,
+		"section_code": 1,
 		"mode": strings.MODES["IND"],		
-		"name": strings.TENSES["IND_PC"]
+		"name": strings.TENSES["IND_PC"]		
 	},
 	{	
 		"code": 2,
+		"section_code": 2,
 		"mode": strings.MODES["IND"],		
-		"name": strings.TENSES["IND_IPF"]
+		"name": strings.TENSES["IND_IPF"]		
 	},	
 	{	
 		"code": 3,
+		"section_code": 3,
 		"mode": strings.MODES["IND"],		
-		"name": strings.TENSES["IND_PQP"]
+		"name": strings.TENSES["IND_PQP"]		
 	},
 	{			
 		"code": 4,
+		"section_code": 4,
 		"mode": strings.MODES["IND"],		
-		"name": strings.TENSES["IND_PS"]
+		"name": strings.TENSES["IND_PS"]		
 	},
 	{	
 		"code": 5,
+		"section_code": 5,
 		"mode": strings.MODES["IND"],		
-		"name": strings.TENSES["IND_PA"]
+		"name": strings.TENSES["IND_PA"]		
 	},
 	{	
 		"code": 6,
+		"section_code": 6,
 		"mode": strings.MODES["IND"],		
-		"name": strings.TENSES["IND_FS"]
+		"name": strings.TENSES["IND_FS"]		
 	},
 	{	
 		"code": 7,
+		"section_code": 7,
 		"mode": strings.MODES["IND"],		
-		"name": strings.TENSES["IND_FA"]
+		"name": strings.TENSES["IND_FA"]		
 	},
 	{	
 		"code": 9,
+		"section_code": 0,
 		"mode": strings.MODES["SUBJ"],
-		"name": strings.TENSES["SUBJ_PRES"]
+		"name": strings.TENSES["SUBJ_PRES"]		
 	},
 	{	
 		"code":  10,
+		"section_code": 1,
 		"mode": strings.MODES["SUBJ"],
-		"name": strings.TENSES["SUBJ_P"]
+		"name": strings.TENSES["SUBJ_P"]		
 	},
 	{	
 		"code": 11,
+		"section_code": 2,
 		"mode": strings.MODES["SUBJ"],
-		"name": strings.TENSES["SUBJ_IMP"]
+		"name": strings.TENSES["SUBJ_IMP"]		
 	},
 	{	
 		"code": 12,
+		"section_code": 3,
 		"mode": strings.MODES["SUBJ"],
-		"name": strings.TENSES["SUBJ_PQP"]
+		"name": strings.TENSES["SUBJ_PQP"]		
 	},
 	{	
 		"code": 14,
+		"section_code": 0,
 		"mode": strings.MODES["COND"],		
-		"name": strings.TENSES["COND_PRES"]
+		"name": strings.TENSES["COND_PRES"]		
 	},
 	{	
 		"code": 15,
+		"section_code": 1,
 		"mode": strings.MODES["COND"],		
-		"name": strings.TENSES["COND_P"]
+		"name": strings.TENSES["COND_P"]		
 	},
 	{	
 		"code": 17,
+		"section_code": 0,
 		"mode": strings.MODES["IMP"],
-		"name": strings.TENSES["IMP_PRES"]
+		"name": strings.TENSES["IMP_PRES"]		
 	},
 	{	
 		"code": 18,
+		"section_code": 1,
 		"mode": strings.MODES["IMP"],
-		"name": strings.TENSES["IMP_P"]
+		"name": strings.TENSES["IMP_P"]		
 	}
 ]
 
@@ -93,19 +109,12 @@ def conjugate(verb, tense):
 
 	tense_obj = get_obj_from_name(tense)	
 	preamble = u"{}\n{} {}\n\n".format(verb, tense_obj['mode'], tense_obj['name'])
-	conjugation = wtf.scrape(verb, tense_obj['code'])
+	conjugation = wtf.scrape(verb=verb, mode=tense_obj['mode'], section_code=tense_obj['section_code'], code=tense_obj['code'])
 	final_conjugation = preamble + conjugation
 
 	return final_conjugation
 
 def get_obj_from_name(tensename):
-	return filter(lambda t: t['name'] == tensename, tenses)[0]
+	# the object returned by filter() is different in Python 2 and Python 3
+	return filter(lambda t: t['name'] == tensename, tenses)[0]	
 
-def get_code_from_name(tensename):
-	return filter(lambda t: t['name'] == tense, tenses)[0]['code']
-
-def get_mode_from_code(tensecode):
-	return filter(lambda t: t['code'] == tensecode, tenses)[0]['mode']
-
-def get_name_from_code(tensecode):
-	return filter(lambda t: t['code'] == tensecode, tenses)[0]['name']
--- language/wiki_telecharger_fr.py
+++ language/wiki_telecharger_fr.py
@@ -14,8 +14,31 @@ tense 15 - table 20
 """
 TABLE_OFFSET = 5
 
-def scrape(verb, tense_code):
+# def scrape_by_code(verb, tense_code):
 
+# 	verb_url = strings.WIKI_FR_CJGTR + verb.lower()
+	
+# 	res = requests.get(verb_url)
+# 	if res.status_code == 404:
+# 		return strings.VERB_NOT_FOUND
+
+# 	html_doc = res.text
+# 	soup = BeautifulSoup(html_doc, 'html.parser')	
+
+# 	scraped_conjugation = u""
+
+# 	# find the right table on the page, and loop through all the rows after the 0th row
+# 	for tr in soup.select("table:nth-of-type(" + str(int(TABLE_OFFSET) + int(tense_code)) + ")")[0].find_all('tr')[1:]:
+# 		if tense_code == 17 or tense_code == 18:
+# 			subject = u""
+# 		else:
+# 			subject = tr.td.get_text().replace(u'\xa0', u' ')
+# 		verb = tr.td.next_sibling.next_sibling.get_text()
+# 		scraped_conjugation += u"{}{}\n".format(subject, verb)
+
+# 	return scraped_conjugation
+
+def scrape(verb, mode, section_code, code):
 	verb_url = strings.WIKI_FR_CJGTR + verb.lower()
 	
 	res = requests.get(verb_url)
@@ -26,11 +49,12 @@ def scrape(verb, tense_code):
 	soup = BeautifulSoup(html_doc, 'html.parser')	
 
 	scraped_conjugation = u""
-
-	# find the right table on the page, and loop through all the rows after the 0th row
-	for tr in soup.select("table:nth-of-type(" + str(int(TABLE_OFFSET) + int(tense_code)) + ")")[0].find_all('tr')[1:]:
-		subject = tr.td.get_text().replace(u'\xa0', u' ')
-		verb = tr.td.next_sibling.next_sibling.string
+	for tr in soup.find(id=mode).find_all_next("table")[section_code + 1].find_all("tr")[1:]:
+		if code == 17 or code == 18:
+			subject = u""
+		else:
+			subject = tr.td.get_text().replace(u'\xa0', u' ')
+		verb = tr.td.next_sibling.next_sibling.get_text()
 		scraped_conjugation += u"{}{}\n".format(subject, verb)
-
+	
 	return scraped_conjugation
