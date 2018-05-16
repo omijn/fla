9234cb41c880aeb595c8a7201dcbc80b894dcbeb
omijn <abhijit.kashyap07@gmail.com>
2018-05-16 14:17:42 -0400

    reformat, handled translations from non-french languages

--- dialogflow.py
+++ dialogflow.py
@@ -16,37 +16,38 @@ from num2words import num2words
 class Parser():
     """Parse incoming requests from Dialogflow"""
 
-    def __init__(self):        
+    def __init__(self):
         self.formatter = Formatter()
 
     def parse(self, data):
         lang = data['queryResult']['languageCode']
         intent = data['queryResult']['intent']['displayName']
-        response = ""        
+        response = ""
 
         if intent == strings.CONJUGATE_INTENT:
             # verb = data['queryResult']['parameters']['frenchVerb']
             response = self.formatter.ask_verb_tense()
-        
+
         elif intent == strings.CONJUGATE_MORE_INTENT:
             response = self.formatter.ask_verb_tense(userWantsMoreOptions=True)
 
         elif intent == strings.CONJUGATE_VERB_INTENT:
             verb = data['queryResult']['outputContexts'][0]['parameters']['frenchVerb']
-            tense = data['queryResult']['queryText']            
+            tense = data['queryResult']['queryText']
             conjugation = language.conjugator.conjugate(verb, tense)
             response = self.formatter.display_conjugation(conjugation)
 
         elif intent == strings.GENDER_INTENT:
             word = data['queryResult']['parameters']['word']
             g = language.gender.gender_fr.get_gender(word)
-            response = self.formatter.display_gender(word, g)            
+            response = self.formatter.display_gender(word, g)
 
         elif intent == strings.NUMERIC_INTENT:
             number = data['queryResult']['parameters']['number']
             word_form_en = num2words(number, lang='en')
             word_form_fr = num2words(number, lang='fr')
-            response = self.formatter.display_number(word_form_en, word_form_fr)
+            response = self.formatter.display_number(
+                word_form_en, word_form_fr)
 
         elif intent == strings.PRONOUNCE_INTENT:
             pass
@@ -72,15 +73,20 @@ class Parser():
                 target = strings.ISO_639_1_FRENCH
             else:
                 target = strings.ISO_639_1_ENGLISH
-            
-            translation = translator.translate(text, source, target)
-            
-            source = strings.LANGUAGES[source]
-            target = strings.LANGUAGES[target]
 
-            response = self.formatter.display_translation(translation, source, target)
-                    
-        return response     
+            translation = translator.translate(text, source, target)
+
+            try:
+                source = strings.LANGUAGES[source]
+                target = strings.LANGUAGES[target]
+            except KeyError:
+                pass
+
+            response = self.formatter.display_translation(
+                translation, source, target)
+
+        return response
+
 
 class Formatter:
     """Format outgoing responses to Dialogflow"""
@@ -94,15 +100,17 @@ class Formatter:
         """
 
         if userWantsMoreOptions:
-            quick_replies = [tense['name'] for tense in language.conjugator.tenses[9:]]
+            quick_replies = [tense['name']
+                             for tense in language.conjugator.tenses[9:]]
         else:
-            quick_replies = [tense['name'] for tense in language.conjugator.tenses[:9]]
+            quick_replies = [tense['name']
+                             for tense in language.conjugator.tenses[:9]]
             quick_replies.append(strings.SHOW_MORE_TENSES)
 
         print(quick_replies)
 
         response = {
-            "fulfillmentText": strings.HOW_TO_CONJ,            
+            "fulfillmentText": strings.HOW_TO_CONJ,
             "fulfillmentMessages": [
                 {
                     "quickReplies": {
@@ -116,12 +124,12 @@ class Formatter:
             "source": strings.DATA_SOURCE,
             "followupEventInput": {}
         }
-        
+
         return json.dumps(response)
 
     def display_conjugation(self, conjugation):
         response = {
-            "fulfillmentText": conjugation,            
+            "fulfillmentText": conjugation,
             "payload": {},
             "outputContexts": [],
             "source": strings.DATA_SOURCE,
@@ -132,12 +140,13 @@ class Formatter:
 
     def display_gender(self, word, gender_code):
         if gender_code in language.gender.gender_fr.genders:
-            msg = u"{} is {}".format(word, language.gender.gender_fr.genders[gender_code])
+            msg = u"{} is {}".format(
+                word, language.gender.gender_fr.genders[gender_code])
         else:
             msg = strings.WORD_NOT_FOUND
 
         response = {
-            "fulfillmentText": msg,            
+            "fulfillmentText": msg,
             "payload": {},
             "outputContexts": [],
             "source": strings.DATA_SOURCE,
@@ -150,7 +159,7 @@ class Formatter:
 
         word_form = u"FR: {}\n\nEN: {}".format(num_fr, num_en)
         response = {
-            "fulfillmentText": word_form,            
+            "fulfillmentText": word_form,
             "payload": {},
             "outputContexts": [],
             "source": strings.DATA_SOURCE,
@@ -161,9 +170,10 @@ class Formatter:
 
     def display_translation(self, translation, source, target):
 
-        trans = u"{}\n\n(translated from {} to {})".format(translation, source, target)
+        trans = u"{}\n\n(translated from {} to {})".format(
+            translation, source, target)
         response = {
-            "fulfillmentText": trans,            
+            "fulfillmentText": trans,
             "payload": {},
             "outputContexts": [],
             "source": strings.DATA_SOURCES["translation"],
